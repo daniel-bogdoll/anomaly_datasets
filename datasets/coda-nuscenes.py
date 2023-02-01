5d1b1754f5fa425cc1c50bb7ceb602ff7234b5f0
SUhlemeyer <suhlemeyer@uni-wuppertal.de>
2023-02-01 17:47:44 +0100

    coda update

--- datasets/coda-nuscenes.py
+++ datasets/coda-nuscenes.py
@@ -5,18 +5,21 @@ import numpy as np
 
 
 class CODANuscenes(Dataset):
-    def __init__(self, root="/datasets/CODA_masks/nuscenes"):
+    def __init__(self, root):
         self.root = root
+        self.images = []
         self.img_labels = []
         self.resolutions = [(900, 1600)]
         self.ood_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-        for im in os.listdir(self.root):
-            self.img_labels.append(os.path.join(self.root, im))
+        for im in os.listdir(self.root): #TODO
+            self.images.append(os.path.join(self.root, im)) #TODO
+            self.img_labels.append(os.path.join(self.root, im)) #TODO
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, i):
+        image = np.array(Image.open(self.images[i]))
         target = np.load(self.img_labels[i])
-        return target
+        return image, target
--- datasets/coda2022-soda10m.py
+++ datasets/coda2022-soda10m.py
@@ -5,18 +5,21 @@ import numpy as np
 
 
 class CODA2022SODA10M(Dataset):
-    def __init__(self, root="/datasets/CODA_masks/2022soda10m"):
+    def __init__(self, root):
         self.root = root
+        self.images = []
         self.img_labels = []
         self.resolutions = [(720, 1280), (720, 958)]
         self.ood_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-        for im in os.listdir(self.root):
-            self.img_labels.append(os.path.join(self.root, im))
+        for im in os.listdir(self.root): #TODO
+            self.images.append(os.path.join(self.root, im)) #TODO
+            self.img_labels.append(os.path.join(self.root, im)) #TODO
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, i):
+        image = np.array(Image.open(self.images[i]))
         target = np.load(self.img_labels[i])
-        return target
+        return image, target
