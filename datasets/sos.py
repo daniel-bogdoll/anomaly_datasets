89da924586ff08ddb33a58a3c98507bc5bb37ea4
SUhlemeyer <suhlemeyer@uni-wuppertal.de>
2023-02-01 16:33:02 +0100

    update wuppertal OOD

--- datasets/sos.py
+++ datasets/sos.py
@@ -5,19 +5,23 @@ import numpy as np
 
 
 class SOS(Dataset):
-    def __init__(self, root="/datasets/semantic_ood"):
+    def __init__(self, root):
         self.root = root
+        self.images = []
         self.img_labels = []
         self.resolutions = [(1080, 1920)]
         self.ood_id = [254]
 
         for seq in os.listdir(root):
-            for im in os.listdir(os.path.join(self.root, seq)):
-                self.img_labels.append(os.path.join(root, seq, im))
+            for im in os.listdir(os.path.join(self.root, seq, 'raw_data')):
+                self.images.append(os.path.join(root, seq, 'raw_data', im))
+                self.img_labels.append(os.path.join(root, seq, 'semantic_ood', im.replace('raw_data.jpg', 'semantic_ood.png')))
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, i):
+        image = np.array(Image.open(self.images[i]))
         target = Image.open(self.img_labels[i]).convert("L")
-        return np.asarray(target, dtype=np.int16)
+        target = np.asarray(target, dtype=np.int16)
+        return image, target
